6ff3c918f7fea96233c0a5c5bdcf58be386ca472
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2024-07-09 10:55:45 -0400

    Update low.py

--- low.py
+++ low.py
@@ -11,17 +11,20 @@ if data.Adj_Close[-1] < min(data.Low)*1.05:
 else:
     print(f"{symbol} is not within 5% low of the year")
 
-# a list of security
-etf_list = pd.read_csv("https://raw.githubusercontent.com/jiahualihuanahuan/stock-data-scraping-analysis-and-prediction/main/CADETF.csv", encoding='latin-1')
-for symbol in etf_list.Symbol:
+# a list of securities
+list_name = "CADETF"
+list = pd.read_csv(f"https://raw.githubusercontent.com/jiahualihuanahuan/stock-data-scraping-analysis-and-prediction/main/{list_name}.csv", encoding='latin-1')
+# list.columns = ["Symbol"]
+for symbol in list.Symbol:
     try:
-        data = yf.download(f"{symbol}", period="max", interval="1d", progress=False)
+        data = yf.download(f"{symbol}", period="1y", interval="1d", progress=False)
         # Add ta features filling NaN values
         data.columns = ["Open", "High", "Low", "Close", "Adj_Close", "Volume"]
-        if data.Adj_Close[-1] < min(data.Low)*1.05:
+        if min(data.Low)*1.01 < data.Adj_Close.iloc[-1] < min(data.Low)*1.05:
             print(f"{symbol} is within 5% low of the year")
+        elif data.Adj_Close.iloc[-1] < min(data.Low)*1.01:
+            print(f"{symbol} is within 1% low of the year")
         else:
-            print(f"{symbol} is not within 5% low of the year")
-
+            pass
     except:
-        print(f"{symbol} error, not screened")
+        pass
